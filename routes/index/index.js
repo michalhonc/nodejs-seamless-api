1e1d777a48222847f3b25c9e321fb98aa87a1661
michalhonc <honc355@gmail.com>
2018-03-07 13:29:16 +0100

    v1.1.1

--- routes/index/index.js
+++ routes/index/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const gameLaunch = require('../rest/endorphina/v1/gameLaunch');
 
 router.get('/', (req, res) => {
-    const launchURL = gameLaunch.real('test', 'common', 'admin', 'somegameid', 'EUR', 'https://example.com');
+    const launchURL = gameLaunch.real('test', 'common', 'admin', 'endorphina_Ninja@ENDORPHINA', 'EUR', 'https://example.com');
     res.send(launchURL);
 })
 
